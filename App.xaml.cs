1d67cb4a3234b5e70afe04f6b899bad06136c457
Mohammed-Alae Zerrou <82812429+Alae-Zer@users.noreply.github.com>
2025-01-08 09:23:19 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ISDP2025_Parfonov_Zerrou
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

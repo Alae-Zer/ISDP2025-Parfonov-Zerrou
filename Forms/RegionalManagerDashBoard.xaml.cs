7e9f9d27cbe09ca1cd43b4235fd58619b50a23af
Mohammed-Alae Zerrou <82812429+Alae-Zer@users.noreply.github.com>
2025-01-17 10:41:50 -0400

    added the other forms

---
+++ Forms/RegionalManagerDashBoard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace ISDP2025_Parfonov_Zerrou
+{
+    /// <summary>
+    /// Interaction logic for AdminDashBoard.xaml
+    /// </summary>
+    public partial class StoreManagerDashBoard : Window
+    {
+        public StoreManagerDashBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}
---
+++ Forms/StoreManagerDashBoard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace ISDP2025_Parfonov_Zerrou
+{
+    /// <summary>
+    /// Interaction logic for AdminDashBoard.xaml
+    /// </summary>
+    public partial class RegionalManagerDashBoard : Window
+    {
+        public RegionalManagerDashBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}

00400911ca497adb3cb4349b1018db81c2010809
SerhiiParfonov <145796006+SerhiiParfonov@users.noreply.github.com>
2025-01-21 12:52:10 -0400

    Uodated And Finished

--- Forms/ForemanUserControls/ForemanInventoryControl.xaml.cs
+++ Forms/ForemanUserControls/ForemanInventoryControl.xaml.cs
@@ -17,6 +17,7 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
         //Global Variables and 
         BestContext context;
         List<string> categoriesList = new List<string>();
+        string selectedImagePath;
         string defaultImagePath = @"D:\WINTER 2025\ISDP\CODE\Images\default.png";
         BitmapImage defaultImage;
 
@@ -31,6 +32,43 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
             btnClear.IsEnabled = false;
             dgvItems.ItemsSource = null;
             LoadDefaultImage();
+        }
+
+        //Assigns Path to String For Further Saving in The Database
+        //Sends Nothing
+        //Returns Nothing
+        private void BrowseImage()
+        {
+            selectedImagePath = string.Empty;
+            string defaultFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
+
+            //Create an Instance wit folder and filters
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = defaultFolder,
+                Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.UriSource = new Uri(openFileDialog.FileName);
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit();
+                    imgProduct.Source = image;
+
+                    selectedImagePath = openFileDialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading selected image: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    imgProduct.Source = defaultImage;
+                    selectedImagePath = string.Empty;
+                }
+            }
         }
 
         //Loads Default Image
@@ -128,29 +166,33 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
             }
         }
 
-        //
-        //
-        //
-        private void LoadItems()
+        //Loads Items To Data Grid
+        //Sends Nothing
+        //Returns Nothing
+        private void LoadItemsToDGV()
         {
             try
             {
+                //QUERY
                 var query = context.Items
                     .Include(i => i.Supplier)
                     .AsQueryable();
 
+                //Apply Filters
                 if (!string.IsNullOrWhiteSpace(txtSearch.Text))
                 {
                     string searchText = txtSearch.Text.ToLower();
                     query = query.Where(i => i.Name.ToLower().Contains(searchText));
                 }
 
+                //Apply Filters
                 if (cmbSearchCategory.SelectedIndex > 0)
                 {
                     string category = cmbSearchCategory.SelectedItem.ToString();
                     query = query.Where(i => i.Category == category);
                 }
 
+                //LOLOLOL When We Found It- We were Shocked how it works
                 var items = query.Select(i => new
                 {
                     i.ItemId,
@@ -166,18 +208,24 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
                     Active = i.Active == 1 ? "Yes" : "No"
                 }).OrderBy(i => i.ItemName).ToList();
 
+                //Bund DataGrid
                 dgvItems.ItemsSource = items;
             }
+            //Cacth
             catch (Exception ex)
             {
                 MessageBox.Show($"Error loading items: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
-        private void PopulateDetails(dynamic selectedItem)
+        //Populates Labels And TextBoxes With Selected row Details
+        //Sends DYNAMIC Object
+        //Returns Nothing
+        private void PopulateDetailFields(dynamic selectedItem)
         {
             try
             {
+                //If Item Is Identified- Display
                 if (selectedItem != null)
                 {
                     lblItemId.Content = selectedItem.ItemId.ToString();
@@ -190,6 +238,7 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
                     txtDescription.Text = selectedItem.Description;
                     txtNotes.Text = selectedItem.Notes;
 
+                    //If Location is not set-Try to load the image using the path from the database
                     if (!string.IsNullOrEmpty(selectedItem.ImageLocation))
                     {
                         try
@@ -199,19 +248,22 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
                             image.UriSource = new Uri(selectedItem.ImageLocation);
                             image.CacheOption = BitmapCacheOption.OnLoad;
                             image.EndInit();
+                            //Set Image in the Alloted Area
                             imgProduct.Source = image;
-
                         }
                         catch
                         {
+                            //Set Default Image if Wasn't able to download
                             imgProduct.Source = defaultImage;
                         }
                     }
                     else
                     {
+                        //If Database doesn't contain Image
                         imgProduct.Source = defaultImage;
                     }
 
+                    //Enable buttons
                     btnClear.IsEnabled = true;
                     btnUpdate.IsEnabled = true;
                 }
@@ -222,8 +274,12 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
             }
         }
 
+        //Reset Inputs adn Set Default Image
+        //Sends Nothing
+        //Returns Nothing
         private void ClearDetails()
         {
+            //Blank Inputs
             lblItemId.Content = string.Empty;
             lblItemName.Content = string.Empty;
             lblSku.Content = string.Empty;
@@ -239,83 +295,80 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
             btnUpdate.IsEnabled = false;
         }
 
+        //Saves Changes 
+        //Sends Nothing
+        //Returns Nothing
         private void SaveChanges()
         {
+            //Create Dialog
             MessageBoxResult result = MessageBox.Show(
                 "Do you want to save these changes?\nYes - Save changes\nNo - Return without saving\nCancel - Continue editing",
                 "Confirmation Needed",
                 MessageBoxButton.YesNoCancel,
                 MessageBoxImage.Question);
 
+            //If User Respondeed YES
             if (result == MessageBoxResult.Yes)
             {
                 try
                 {
-                    if (string.IsNullOrEmpty(lblItemId.Content?.ToString()))
+                    //Verify If Item Id Is Not Empty
+                    if (string.IsNullOrEmpty(lblItemId.Content.ToString()))
                     {
                         MessageBox.Show("No item selected.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                         return;
                     }
 
-                    int itemId = int.Parse(lblItemId.Content.ToString());
+                    //Parse Label To Integer and QUERY
+                    int.TryParse(lblItemId.Content.ToString(), out int itemId);
                     var item = context.Items.Find(itemId);
 
+                    //If Item Is Found
                     if (item != null)
                     {
+                        //Assign Values and Save Changes
                         item.Description = txtDescription.Text;
                         item.Notes = txtNotes.Text;
 
-
+                        //If Image Was Selected
+                        if (!string.IsNullOrEmpty(selectedImagePath))
+                        {
+                            item.ImageLocation = selectedImagePath;
+                        }
+                        //Save
                         context.SaveChanges();
+
+                        //Success Confirmation
                         MessageBox.Show("Changes saved successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        //Refresh fields For the Next Step
                         EnableEditing(false);
-                        LoadItems();
+                        LoadItemsToDGV();
                     }
                 }
+                //Catch Exeption
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Error saving changes: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+            //Reset Inputs and DGV Selection
             else if (result == MessageBoxResult.No)
             {
                 EnableEditing(false);
+                ClearDetails();
+                selectedImagePath = string.Empty;
             }
         }
 
         private void BtnBrowseImage_Click(object sender, RoutedEventArgs e)
         {
-            string initialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
-
-            OpenFileDialog openFileDialog = new OpenFileDialog
-            {
-                InitialDirectory = initialDirectory,
-                Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*"
-            };
-
-            if (openFileDialog.ShowDialog() == true)
-            {
-                try
-                {
-                    var image = new BitmapImage();
-                    image.BeginInit();
-                    image.UriSource = new Uri(openFileDialog.FileName);
-                    image.CacheOption = BitmapCacheOption.OnLoad;
-                    image.EndInit();
-                    imgProduct.Source = image;
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error loading selected image: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    imgProduct.Source = defaultImage;
-                }
-            }
+            BrowseImage();
         }
 
         private void BtnRefresh_Click(object sender, RoutedEventArgs e)
         {
-            LoadItems();
+            LoadItemsToDGV();
             ClearDetails();
             EnableEditing(false);
             EnableSearchInputs(true);
@@ -329,15 +382,17 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
 
         private void DgInventory_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //If item Exists- Display
             if (dgvItems.SelectedItem != null)
             {
-                PopulateDetails(dgvItems.SelectedItem);
+                PopulateDetailFields(dgvItems.SelectedItem);
                 EnableEditing(false);
             }
         }
 
         private void BtnUpdate_Click(object sender, RoutedEventArgs e)
         {
+            //Verify that DGV already have an Item Source
             if (dgvItems.ItemsSource == null)
             {
                 MessageBox.Show("Please click Refresh to load the data first", "Attention",
@@ -345,7 +400,7 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
                 return;
             }
 
-            if (string.IsNullOrEmpty(lblItemId.Content?.ToString()))
+            if (lblItemId.Content == null || lblItemId.Content.ToString() == "")
             {
                 MessageBox.Show("Please select an item first.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
@@ -360,27 +415,38 @@ namespace ISDP2025_Parfonov_Zerrou.Forms.ForemanUserControls
 
         private void TxtSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            btnClear.IsEnabled = !string.IsNullOrWhiteSpace(txtSearch.Text);
+            //Enable Clear Button if something in Search Field
+            if (txtSearch.Text == "" || txtSearch.Text == null)
+            {
+                btnClear.IsEnabled = false;
+            }
+            else
+            {
+                btnClear.IsEnabled = true;
+            }
+
             if (dgvItems.ItemsSource != null)
             {
-                LoadItems();
+                LoadItemsToDGV();
             }
         }
 
         private void CmbSearchCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //Enable Clear Button If Selected index is Not Default
             if (cmbSearchCategory.SelectedIndex > 0)
             {
                 btnClear.IsEnabled = true;
             }
             if (dgvItems.ItemsSource != null)
             {
-                LoadItems();
+                LoadItemsToDGV();
             }
         }
 
         private void UserControl_Unloaded(object sender, RoutedEventArgs e)
         {
+            //If Problem With Loading Has Occured - Disconnect Database
             if (context != null)
             {
                 context.Dispose();
